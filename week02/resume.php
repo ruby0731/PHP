ae704e7ba7a9fefcd48590198636f255f3109f44
ruby0731 <125986262+ruby0731@users.noreply.github.com>
2024-03-21 09:31:30 +0800

    Create resume.php

---
+++ week02/resume.php
@@ -0,0 +1,28 @@
+<html>
+
+<head>
+
+<meta chaset = "utf-8">
+<meta name = "履歷, 履歷表">
+<title>履歷表</title>
+
+</head>
+<body bgcolor = "#FFFFE9">
+
+<center> <b> <font size = "7"> <font color = "black"> 履歷表 </font> </font> </b> </center>
+<hr color = "white" width = "80%" size = "3px">
+<center> <img src = "iu.jpg"></center>
+<center> <b> <font size = "5"> <font color = "black"> 李知恩 </font> </font> </b> </center>
+<h1> <font size = "5"> <font color = "black"> <font face = "微軟正黑體"> 一、基本資料 </font></h1>
+
+<center><table style = "border:1px solid black" border = "1" cellspacing = "0">
+
+    <tr><th>姓名</th><th>李知恩</th><th>聯絡電話</th><th>0909911192</th></tr>
+    <tr><th>通訊地址</th><th>高雄市楠梓區大學三十二街392號</th><th>電子郵件</th><th>a1113321@mail.nuk.edu.tw</th></tr>
+    <tr><th>學歷</th><th>高雄大學</th><th>工作經驗</th><th>數學家教、教育部數位學伴</th></tr>
+</table></center>
+<h2> <font size = "5"> <font color = "black"> <font face = "微軟正黑體"> 二、自傳 </font></h2>
+<h3> <font size = "3"> <font color = "black"> <font face = "微軟正黑體"> &nbsp&nbsp&nbsp&nbsp&nbsp今天要打的是自傳，但是主要內容我不知道要打什麼，而且也不知道他到底會不會主動換行，ㄟ...不知道最前面能不能用tabㄟ，真酷，看來只能先這樣了。</font></h2>
+</body>
+
+</html>
