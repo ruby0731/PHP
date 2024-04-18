8c1cd5c56839f07b3ba72fedb56aeec1d3e93084
ruby0731 <125986262+ruby0731@users.noreply.github.com>
2024-04-18 11:34:14 +0800

    Create chair.php

---
+++ php2024midterm/chair.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+?>
+<html>
+<head>
+<meta charset = "utf8">
+<title>Chair登入介面</title>
+</head>
+<body>
+<center> <b> <font size = "8"> <font color = "black">  Chair您好，歡迎進入論文評論網頁 </font> </font> </b></center> 
+
+</html>
