4d9838168f5d74c7f01a4d85bed9dd46dc8702fd
ruby0731 <125986262+ruby0731@users.noreply.github.com>
2024-06-15 14:54:02 +0800

    Create upload.php

---
+++ week0509/upload.php
@@ -0,0 +1,7 @@
+<meta charset = 'utf8'>
+<form action = "uploadcheck.php" method = "post" enctype = "multipart/form-data">
+
+請選擇檔案：<input type = "file" name = "myFile"><br/>
+
+<input type = "submit">
+</form>
