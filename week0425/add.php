f437cb12dff011e7d5e7e6c6841c26f7c81bef28
ruby0731 <125986262+ruby0731@users.noreply.github.com>
2024-06-15 14:31:24 +0800

    Create add.php

---
+++ week0425/add.php
@@ -0,0 +1,7 @@
+<meta charest = 'utf8'>
+
+<form action = "addDB.php" method = "post">
+<input type = "text" name = "sName"><br/>
+<input type = "text" name = "sDept"><br/>
+<input type = "submit">
+</form>
